886e9eb0217cd4352c1385b980cbeeb7ab9c98d8
Julia-Park <julia.ay.park@gmail.com>
2022-06-27 11:43:27 -0400

    Set up manager and Contact initialization

--- public/javascripts/frontend_manager.js
+++ public/javascripts/frontend_manager.js
@@ -1,20 +1,42 @@
 class Contact {
-  constructor() {
-
+  constructor(data) {
+    this.id = data.id;
+    this.fullName = data.full_name;
+    this.email = data.email;
+    this.phoneNumber = data.phone_number;
+    this.tags = data.tags;
   }
 }
 
 class Manager {
   constructor() {
+    this.contacts = [];
+    this.initializeContacts();
 
+    // this.view = new View();
+  }
+
+  initializeContacts() { // fetch contacts, create contact objects
+    fetch('/api/contacts').then(async response => {
+      let data;
+
+      if (response.status === 200) {
+        data = await response.json();
+        data.forEach(entry => this.contacts.push(new Contact(entry)));
+      }
+      
+      console.log(this.contacts);
+    });
   }
 }
 
-class Renderer {
+class View {
   constructor() {
-
+    // this.render();
   }
 }
+
+new Manager();
 
 /*
 Initial flow:
